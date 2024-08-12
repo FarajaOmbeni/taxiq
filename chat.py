4c8e976808a6a64d56d0ecbee186676688d2af72
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2024-08-12 19:19:04 +0300

    first commit

--- chat.py
+++ chat.py
@@ -1,18 +1,17 @@
 import random
 import json
-
-import torch
+import tensorflow as tf
 
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = tf.device('cuda' if tf.cuda.is_available() else 'cpu')
 
 with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
 FILE = "data.pth"
-data = torch.load(FILE)
+data = tf.load(FILE)
 
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
@@ -31,14 +30,14 @@ def get_response(msg):
     sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
+    X = tf.from_numpy(X).to(device)
 
     output = model(X)
-    _, predicted = torch.max(output, dim=1)
+    _, predicted = tf.max(output, dim=1)
 
     tag = tags[predicted.item()]
 
-    probs = torch.softmax(output, dim=1)
+    probs = tf.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
     if prob.item() > 0.75:
         for intent in intents['intents']:
